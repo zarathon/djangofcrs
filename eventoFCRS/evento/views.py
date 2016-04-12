8e3c013cdf38505c537b46e0f4f18f6eefb69bdf
Zarathon Maia <zarathonmaia@gmail.com>
2016-04-12 14:51:08 -0300

    Passando parametro para o Template

--- eventoFCRS/evento/views.py
+++ eventoFCRS/evento/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Fucking World!")
+    return render(request, 'evento/home.html', {
+        'nome': "Zarathon Maia",
+        'idade': '30 anos',
+        'altura': '1,92 metros',
+        })
